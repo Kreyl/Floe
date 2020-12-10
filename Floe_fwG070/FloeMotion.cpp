59b74037e2b691ade250b6bee32ea2b1e055fff9
kreyl <laystoll@yandex.ru>
2020-12-10 23:02:35 +0300

    Algo2

--- Floe_fwG070/FloeMotion.cpp
+++ Floe_fwG070/FloeMotion.cpp
@@ -6,73 +6,146 @@
  */
 
 #include "FloeMotion.h"
-#include <stdint.h>
 #include "kl_i2cG070.h"
 #include "ch.h"
 
 static thread_reference_t ThdRef = nullptr;
-static int16_t ax[3] = { 0 };
+
+class Acc_t {
+public:
+    int16_t x[3] = { 0 };
+    Acc_t() {}
+    Acc_t(int16_t a0, int16_t a1, int16_t a2) { x[0] = a0; x[1] = a1; x[2] = a2; }
+    void Print() { Printf("%d\t%d\t%d\r\n", x[0],x[1],x[2]); }
+
+    Acc_t& operator = (const Acc_t& R) {
+        x[0] = R.x[0];
+        x[1] = R.x[1];
+        x[2] = R.x[2];
+        return *this;
+    }
+
+    int16_t& operator[](const int32_t Indx) { return x[Indx]; }
+
+    friend Acc_t operator - (const Acc_t &L, const Acc_t &R) {
+        return Acc_t(L.x[0] - R.x[0], L.x[1] - R.x[1], L.x[2] - R.x[2]);
+    }
+
+//    friend Acc_t operator * (const int32_t v) {
+//        return Acc_t(
+//    }
+
+    bool AllLessThan(const int32_t R) {
+        if(x[0] >= R) return false;
+        if(x[1] >= R) return false;
+        if(x[2] >= R) return false;
+        return true;
+    }
+
+    bool AnyBiggerThan(const Acc_t &R) {
+        if(x[0] > R.x[0]) return true;
+        if(x[1] > R.x[1]) return true;
+        if(x[2] > R.x[2]) return true;
+        return false;
+    }
+
+    Acc_t Percent(int32_t Percent) {
+        return Acc_t(((int32_t)x[0] * Percent) / 100L, ((int32_t)x[1] * Percent) / 100L, ((int32_t)x[2] * Percent) / 100L);
+    }
+};
+
+Acc_t Module(Acc_t Acc) {
+    if(Acc[0] < 0) Acc[0] = - Acc[0];
+    if(Acc[1] < 0) Acc[1] = - Acc[1];
+    if(Acc[2] < 0) Acc[2] = - Acc[2];
+    return Acc;
+}
+
+static Acc_t vNow, vPrev, vMid;
+static Acc_t vDifVal;
+
 void AcgIrqHandler();
 static const PinIrq_t IIrq{ACG_IRQ_PIN, pudPullDown, AcgIrqHandler};
 
-namespace Motion {
-#define a3mid               -4095
+//#define a3mid               -4095
 // ==== Knock params ====
-// Change this:
-#define KnockStartThreshold 1206L
-#define KnockEndThreshold   108L
-#define KnockDurationHigh   10L
-#define KnockDurationLow    3L
+#define DifPercent          50L
+#define Delta               100L
+#define MaxKnockLength      5L
 
-
-enum State_t { mstIdle, mstKnockWaitingHigh, mstKnockWaitingLow } State = mstIdle;
-
-inline int32_t a3dif() { return (ax[2] > a3mid)? (ax[2] - a3mid) : (a3mid - ax[2]); }
+enum State_t { mstMoving, mstIdle, mstWaiting, mstKnockWaitingLow } State = mstMoving;
 
 int32_t DurationHigh = 0, DurationLow = 0;
+int32_t CalmCounter = 0;
 
-void Update() {
-//        Printf("%d\t%d\t%d\r\n", ax[0],ax[1],ax[2]);
+static void Update() {
     switch(State) {
+        case mstMoving:
+//            Printf("Mv\r");
+            if(Module(vNow - vPrev).AllLessThan(Delta)) {
+                CalmCounter++;
+                if(CalmCounter >= 10) {
+                    State = mstIdle;
+                    vMid = vNow;
+                    // Calculate values to compare with
+                    vDifVal = Module(vMid.Percent(DifPercent));
+                    Printf("vDifVal ");
+                    vDifVal.Print();
+                    CalmCounter = 0;
+                }
+            }
+            else CalmCounter = 0;
+            break;
+
         case mstIdle:
-            Printf("dif1: %d\r", a3dif());
-            if(a3dif() > KnockStartThreshold) {
-                State = mstKnockWaitingHigh;
+//            Printf("Idle\r");
+            if(Module(vNow - vMid).AnyBiggerThan(vDifVal)) {
+                State = mstWaiting;
                 DurationHigh = 0;
+                Printf("Mod ");
+                Module(vNow - vMid).Print();
             }
             break;
 
-        case mstKnockWaitingHigh:
+        case mstWaiting:
+//            Printf("Wait\r");
             DurationHigh++;
-            // Every KnockDurationHigh, check if calmed down
-            if(DurationHigh % KnockDurationHigh == 0) {
-                Printf("dif2: %d\r", a3dif());
-                if(a3dif() < KnockEndThreshold) {
-                    State = mstKnockWaitingLow;
-                    DurationLow = 0;
+            if(DurationHigh >= MaxKnockLength) {
+                // Check if calmed down
+                if(Module(vNow - vPrev).AllLessThan(Delta)) {
+                    CalmCounter++;
+                    if(CalmCounter >= 3) { // Knock occured
+                        Printf("Knock\r");
+                        State = mstMoving;
+                        CalmCounter = 0;
+                    }
                 }
+                else { // Not calm
+                    State = mstMoving;
+                    CalmCounter = 0;
+                }
+                Printf("ModW ");
+                Module(vNow - vPrev).Print();
             }
             break;
 
         case mstKnockWaitingLow:
-            Printf("dif3: %d\r", a3dif());
-            if(a3dif() < KnockEndThreshold) {
-                DurationLow++;
-                if(DurationLow >= KnockDurationLow) { // Calmed down
-                    State = mstIdle;
-                    Printf("DurationHigh: %d\r", DurationHigh);
-//                    if(DurationHigh
-//                    Printf(
-                }
-            }
-            else State = mstKnockWaitingHigh;
+//            Printf("dif3: %d\r", a3dif());
+//            if(a3dif() < KnockEndThreshold) {
+//                DurationLow++;
+//                if(DurationLow >= KnockDurationLow) { // Calmed down
+//                    State = mstIdle;
+//                    Printf("DurationHigh: %d\r", DurationHigh);
+////                    if(DurationHigh
+////                    Printf(
+//                }
+//            }
+//            else State = mstKnockWaitingHigh;
             break;
     } // switch
-
+    // Save new values
+    vPrev = vNow;
 }
-
-};
-
 
 #if 1 // ============================ LIS3D ====================================
 #define LIS_I2C_ADDR        0x19
@@ -100,6 +173,7 @@ void Update() {
 static uint8_t ReadReg(uint8_t RegAddr, uint8_t *Value) {
     return acgi2c.WriteRead(LIS_I2C_ADDR, &RegAddr, 1, Value, 1);
 }
+
 static uint8_t WriteReg(uint8_t RegAddr, uint8_t Value) {
     uint8_t arr[2];
     arr[0] = RegAddr;
@@ -109,7 +183,7 @@ static uint8_t WriteReg(uint8_t RegAddr, uint8_t Value) {
 
 static uint8_t ReadData() {
     uint8_t RegAddr = LIS_RA_OUT_X_L | 0x80;
-    return acgi2c.WriteRead(LIS_I2C_ADDR, &RegAddr, 1, (uint8_t*)ax, 6);
+    return acgi2c.WriteRead(LIS_I2C_ADDR, &RegAddr, 1, (uint8_t*)vNow.x, 6);
 }
 #endif
 
@@ -121,7 +195,7 @@ static void AcgThread(void *arg) {
         chSysLock();
         chThdSuspendS(&ThdRef); // Wait IRQ
         chSysUnlock();
-        if(ReadData() == retvOk) Motion::Update();
+        if(ReadData() == retvOk) Update();
         else Printf("Err\r");
     }
 }
